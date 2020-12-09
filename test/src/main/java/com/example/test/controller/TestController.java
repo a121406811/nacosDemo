3e6acc46f87faa66902f5c3aba9542d44d5dbafb
liangpan <lp10050@qq.com>
2020-12-09 15:32:35 +0800

    基础整合完毕   1、网关是独立pom，其他server引用了父pom   2、网关routes配置都引用了nacos里面的配置 注意： 1、controller里面，需要被加载的方法必须有@ApiOperation注解 2、如果配置使用yaml，那么文件名必须为bootstrap.yml，否则application只加载properties类的文件

--- test/src/main/java/com/example/test/controller/TestController.java
+++ test/src/main/java/com/example/test/controller/TestController.java
@@ -1,6 +1,5 @@
-package com.example.nacos.controller;
+package com.example.test.controller;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @Api(value = "测试接口")
-@RequestMapping("/nacos")
+@RequestMapping("/test")
 @RestController
 @RefreshScope
 public class TestController {
@@ -19,16 +18,9 @@ public class TestController {
     @Value("${name}")
     private String name;
 
-//    @ApiOperation(value = "这是一个hello接口", notes = "接口发布说明")
-//    @GetMapping("/hello")
-//    public String hello() {
-//        return "hello";
-//    }
-//
-
-    @ApiOperation(value = "这是一个hello接口")     //这个注解必须要
+    @ApiOperation(value = "test")     //这个注解必须要
     @GetMapping("/test")
     public String test() {
-        return "nacos:" + name + "1111";
+        return name;
     }
 }
